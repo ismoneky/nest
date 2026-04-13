f5ae2013c9d7fd4ccdf0e4cf0222fe9060c77d3d
ismoneky <2903652220@qq.com>
2026-04-14 00:41:55 +0800

    微信支付

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,5 @@ async function bootstrap() {
   const port = process.env.PORT || '3000';
   await app.listen(port, '0.0.0.0');
 
-  console.log(`Application is running on: http://localhost:${port}`);
-  console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 }
 bootstrap();
---
+++ upload.sh
@@ -0,0 +1,10 @@
+scp -r /Users/zhangzhiwei/Desktop/code/copy/nest/package.json root@82.157.111.208:/app/backend/package.json
+
+scp -r /Users/zhangzhiwei/Desktop/code/copy/nest/package-lock.json root@82.157.111.208:/app/backend/package-lock.json
+
+scp -r /Users/zhangzhiwei/Desktop/code/copy/nest/dist/* root@82.157.111.208:/app/backend/dist/
+
+scp -r /Users/zhangzhiwei/Desktop/code/copy/nest/certs/* root@82.157.111.208:/app/backend/certs/
+
+scp -r /Users/zhangzhiwei/Desktop/code/copy/nest/.env root@82.157.111.208:/app/backend/.env
+
