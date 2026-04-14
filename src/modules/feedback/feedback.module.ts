62e0c9ec21ba702654b898abfcc55731a1ff4357
ismoneky <2903652220@qq.com>
2026-04-14 20:56:16 +0800

    补充文件导入

--- src/modules/feedback/feedback.module.ts
+++ src/modules/feedback/feedback.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
 import { Feedback } from '../../entities/feedback.entity';
 import { FeedbackRepository } from '../../repositories/feedback.repository';
 import { FeedbackService } from './feedback.service';
 import { FeedbackController } from './feedback.controller';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Feedback])],
+    imports: [TypeOrmModule.forFeature([Feedback]), UserModule],
     controllers: [FeedbackController],
     providers: [FeedbackService, FeedbackRepository],
 })
